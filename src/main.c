f311712ab813c23d70431ebb6bb08d9c4221aa46
ZeppIine <kimmin0904@gmail.com>
2023-12-09 15:13:11 +0900

    test nfc code

--- src/main.c
+++ src/main.c
@@ -1,13 +1,25 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <softPwm.h>
-#include <wiringPi.h>
-#include <wiringPiI2C.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+// #include <unistd.h>
+// #include <softPwm.h>
+// #include <wiringPi.h>
+// #include <wiringPiI2C.h>
+// #include <nfc/nfc.h>
 #include "rgb_led.h"
+#include "nfc.h"
 
 int main(void)
 {
-    wiringPiSetupGpio();
-    RGBled();
+    // wiringPi 초기화
+    if (wiringPiSetupGpio() == -1) {
+        fprintf(stderr, "wiringPi 초기화에 실패했습니다.\n");
+        return 1;
+    }
+    // PN532 초기화 및 설정
+    initializePn532();
+    // NFC 카드 읽기
+    readNfcCard();
+    // RGBled();
     return 0;
 }
