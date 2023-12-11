9ff236aeb6ab02ab1d5551b99a8fb3e95d190eb9
ZeppIine <kimmin0904@gmail.com>
2023-12-11 13:04:50 +0900

    RGBLed 함수 인자 생성

--- src/rgb_led.c
+++ src/rgb_led.c
@@ -25,21 +25,23 @@ void ledColorSet(UC R_no, UC G_no, UC B_no)
     softPwmWrite(B_PIO, B_no);
 }
 
-void RGBled()
+void RGBled(UC R_no, UC G_no, UC B_no)
 {
     pinMode(R_PIO, OUTPUT);
     pinMode(G_PIO, OUTPUT);
     pinMode(B_PIO, OUTPUT);
     ledInit();
-    int cont = 1;
-    UC R_no, G_no, B_no;
-    while (cont)
-    {
-        printf("R, G, B : ");
-        scanf("%hhu %hhu %hhu", &R_no, &G_no, &B_no);
+    // int cont = 1;
+    // UC R_no, G_no, B_no;
+    // while (cont)
+    // {
+        // printf("R, G, B : ");
+        // scanf("%hhu %hhu %hhu", &R_no, &G_no, &B_no);
         printf("R: %u, G: %u, B: %u\n", R_no, G_no, B_no);
         ledColorSet(R_no, G_no, B_no);
-        if (R_no + G_no + B_no == 0)
-            cont = 0;
-    }
+    //     if (R_no + G_no + B_no == 0)
+    //         cont = 0;
+    // }
+    delay(1000);
+    ledColorSet(255, 255, 255);
 }
