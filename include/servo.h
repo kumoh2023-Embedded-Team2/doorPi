adcbb531733463b2355e85a1a046df32900045db
ZeppIine <kimmin0904@gmail.com>
2023-12-13 23:06:51 +0900

    서보모터 간섭 제거

--- include/servo.h
+++ include/servo.h
@@ -1,12 +1,14 @@
 #include <wiringPi.h>
+#include <softPwm.h>
 #include <stdio.h>
 #include <stdlib.h> /* atoi( ) */
 
-#define SERVO_PIN 19
+#define SERVO_PIN 20
 
-#define MAX_DEGREE 120
-#define NEUTRAL_DEGREE 75
-#define MIN_DEGREE 30
+#define SERVO_RANGE 200
+#define MAX_DEGREE 25
+#define NEUTRAL_DEGREE 4
+#define MIN_DEGREE 5
 
 void servoInit(void);
 
--- src/servo.c
+++ src/servo.c
@@ -1,26 +1,21 @@
 #include <wiringPi.h>
+#include <softPwm.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "servo.h"
 
 void servoInit()
 {
-    int range = 1000;
-    int divisor = 384;
-
-    pinMode(SERVO_PIN, PWM_OUTPUT);
-    pwmSetMode(PWM_MODE_MS);
-    pwmSetRange(range);
-    pwmSetClock(divisor);
-    
-    pwmWrite(SERVO_PIN, NEUTRAL_DEGREE);
+    pinMode(SERVO_PIN, OUTPUT);
+    softPwmCreate(SERVO_PIN, NEUTRAL_DEGREE, SERVO_RANGE);
 }
 
 void servoOpen()
 {
-    pwmWrite(SERVO_PIN, MAX_DEGREE);
+    softPwmWrite(SERVO_PIN, MAX_DEGREE);
     printf("열림 ㅅㄱ\n");
     delay(5000);
-    pwmWrite(SERVO_PIN, NEUTRAL_DEGREE);
+
+    softPwmWrite(SERVO_PIN, NEUTRAL_DEGREE);
     printf("닫힘 ㅅㄱ\n");
 }
